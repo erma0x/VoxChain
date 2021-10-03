87500235a344c8a91c704395dbc4bad06b66a761
Ermano B <35337869+erma0x@users.noreply.github.com>
2021-10-03 13:17:41 +0200

    Update main function

--- voxchain.py
+++ voxchain.py
@@ -42,33 +42,46 @@ class VoxBlockChain:
     def show_last_block(self):
         print(""" block number {0} time {1} content {2} """.format(self.block_number,self.chain[-1]['timestamp'],self.chain[-1]['content'] ))
     
-        
-# Users
-users = ['Jhon','Malcom','Selly','David','Linda','Andy','Margaret','Lisa','Simon']       
- 
- 
-# KEYS
-# admin_key = 'dCXoi1290sa21n9a' # future improvement
-proof = 42
-
-
-# Start blockchain
-blockchain = VoxBlockChain() # object creation
-first_block = blockchain.init_chain() # parameters initialization
-blockchain.add_block(first_block)
-
-print('Blockchain up and running ...')
 
 if __name__ == '__main__':
+
+    # Users
+    users = ['Jhon','Malcom','Selly','David','Linda','Andy','Margaret','Lisa','Simon']       
+    timing_between_block = 2 # seconds
+    
+    # KEYS
+    # admin_key = 'dCXoi1290sa21n9a' # future improvement
+    proof = 42
+
+    # Start blockchain
+    blockchain = VoxBlockChain() # object creation
+    first_block = blockchain.init_chain() # parameters initialization
+    blockchain.add_block(first_block)
+
+    print('Blockchain up and running ...')
+
     while True:
         if new_transaction==True:
+
+            # This part is generated form random users just for simulation
+            # this part to be integrated such P2P servers for get() and post() block data into the shared blockchain
+            # containing the real data for the sender, reciver and the amount.
             sender = rnd.choice(users) # RANDOM BLOCKCHAIN CONTENT , this could be a transaction, strat/end of some service or another DeFi App
             reciver = rnd.choice(users)
             amount=rnd.random()
-            block_content = '{0} has send to {1}  \t\t {2} of vox coin'.format(sender,reciver,round(amount,7))
 
-            next_block = blockchain.create_block(proof,previous_hash=blockchain.previous_hash_block, block_data=block_content) 
+
+            # message in the blockchain
+            block_content = '{0} has send to {1}  \t\t {2} of vox coin'.format(sender,reciver,round(amount,7))
+            
+            # compute next block of the blockchain
+            next_block = blockchain.create_block(proof, previous_hash=blockchain.previous_hash_block, block_data=block_content) 
+            
+            # add block to the blockchain
             blockchain.add_block(next_block)
 
+            # show me 
             blockchain.show_last_block()
-            time.sleep(2)
+            
+            # block time in the blockchain 
+            time.sleep(timing_between_block)
